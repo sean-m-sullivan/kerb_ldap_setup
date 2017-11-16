ba998ccdb43c1ec3409be7a06b8ee50b1ff9f8cf
Wilk42 <ssulliva@redhat.com>
2017-11-16 16:50:27 -0500

    fix Windows

--- files/winrm.ps1
+++ files/winrm.ps1
@@ -1,5 +1,10 @@
-﻿Enable-PSRemoting –force
+Enable-PSRemoting –force
 winrm set winrm/config/service/auth '@{Basic="false"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="false"}'
 winrm set winrm/config/client/auth '@{Basic="false"}'
 winrm set winrm/config/client '@{AllowUnencrypted="false"}'
+winrm set winrm/config/client/auth '@{Kerberos="true"}'
+winrm set winrm/config/client '@{Negotiate="true"}'
+winrm set winrm/config/client '@{Certificate="true"}'
+winrm set winrm/config/service/auth '@{Kerberos="true"}'
+winrm set winrm/config/service '@{Negotiate="true"}'
